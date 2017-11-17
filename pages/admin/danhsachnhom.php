972e2e6e5332d6cc749c078a57ae3c339083febc
DESKTOP-6IUJ8RK\KienDuong <asus12121996@gmail.com>
2017-11-17 16:44:21 +0700

    UPDATE CODE ADMIN

--- pages/admin/danhsachnhom.php
+++ pages/admin/danhsachnhom.php
@@ -1,35 +1,54 @@
-<?php
-$sql = "SELECT * FROM _groups" ;
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-?>
-<div class="box-body">
-              <table id="example2" class="table table-bordered table-hover">
-                <thead>
-                <tr>
-                  <th>Mã nhóm</th>
-                  <th>Tên Nhóm</th>
-                  <th>Số lượng</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php
-                 while($row = $result->fetch_assoc()) {
-                ?>
-                <tr>
-                  <td><?php echo $row['_MANHOM']?></td>
-                  <td><?php echo $row['_TENNHOM']?></td>
-                  <td><?php echo $row['_SOLUONG']?></td>
-                </tr>
-                <?php
-                  }
-                } else {
-                    echo "0 results";
-                }
-                $conn->close();
-                ?>
-                </tbody>
-              </table>
+<div class="row">
+  <div class="col-xs-12">
+    <div class="box">
+      <div class="box-header">
+        <h3 class="box-title">DANH SÁCH NHÓM</h3>
+      </div>
+      <!-- /.box-header -->
+      <div class="box-body">
+        <table id="tabledanhsachnhom" class="table table-bordered table-hover">
+          <thead>
+          <tr>
+            <th>MÃ NHÓM</th>
+            <th>TÊN NHÓM</th>
+            <th>SỐ LƯỢNG</th>
+          </tr>
+          </thead>
+          <tbody>
+            <?php 
+              $sql = "SELECT * FROM _groups";
+              $result = mysqli_query($conn,$sql);
+              if(mysqli_num_rows($result) == FALSE)
+              {}
+              else
+              {
+                while($row = mysqli_fetch_row($result))
+                {
+                  echo "<tr>";
+                  echo "<td>";
+                  // mã nhóm
+                  echo "$row[0]";
+                  echo "</td>";
+                  echo "<td>";
+                  // tên nhóm
+                  echo "$row[1]";
+                  echo "</td>";
+                  echo "<td>";
+                  // số lượng
+                  echo "$row[2]";
+                  echo "</td>";
+                  echo "</tr>";
+                } 
+              }
+              mysqli_free_result($result);
+              mysqli_close($conn);
+            ?>
+          </tbody>
+        </table>
+      </div>
+      <!-- /.box-body -->
+    </div>
+    <!-- /.box -->
+  </div>
+  <!-- /.col -->
 </div>
--- pages/admin/danhsachthanhvien.php
+++ pages/admin/danhsachthanhvien.php
@@ -1,43 +1,74 @@
-<?php
-$sql = "SELECT * FROM (_account ac inner join _users us on ac._id= us._id) inner join _groups gr on us._manhom=gr._manhom";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-?>
-<div class="box-body">
-              <table id="example2" class="table table-bordered table-hover">
-                <thead>
-                <tr>
-                  <th>Họ</th>
-                  <th>Tên</th>
-                  <th>Giới Tính</th>
-                  <th>Ngày Sinh</th>
-                  <th>Email</th>
-                  <th>User</th>
-                  <th>Nhóm</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php
-                 while($row = $result->fetch_assoc()) {
-                ?>
-                <tr>
-                  <td><?php echo $row['_HO']?></td>
-                  <td><?php echo $row['_TEN']?></td>
-                  <td><?php echo $row['_GIOITINH']?></td>
-                  <td><?php echo $row['_NGAYSINH']?></td>
-                  <td><?php echo $row['_EMAIL']?></td>
-                  <td><?php echo $row['_USERNAME']?></td>
-                  <td><?php echo $row['_TENNHOM']?></td>
-                </tr>
-                <?php
-                  }
-                } else {
-                    echo "0 results";
-                }
-                $conn->close();
-                ?>
-                </tbody>
-              </table>
+<div class="row">
+  <div class="col-xs-12">
+    <div class="box">
+      <div class="box-header">
+        <h3 class="box-title">DANH SÁCH THÀNH VIÊN</h3>
+      </div>
+      <!-- /.box-header -->
+      <div class="box-body">
+        <table id="tabledanhsachthanhvien" class="table table-bordered table-hover">
+          <thead>
+          <tr>
+            <th>HỌ</th>
+            <th>TÊN</th>
+            <th>GIỚI TÍNH</th>
+            <th>NGÀY SINH</th>
+            <th>EMAIL</th>
+            <th>TÀI KHOẢN</th>
+            <th>NHÓM</th>
+          </tr>
+          </thead>
+          <tbody>
+            <?php 
+              $sql = "SELECT _users._HO,_users._TEN,CASE WHEN _users._GIOITINH = 1 THEN 'Nam' WHEN _users._GIOITINH = 0 THEN N'Nữ' END,_users._NGAYSINH,_users._EMAIL,_account._USERNAME,_groups._TENNHOM FROM _users,_account,_groups WHERE _groups._MANHOM = _users._MANHOM AND _users._ID = _account._ID";
+              $result = mysqli_query($conn,$sql);
+              if(mysqli_num_rows($result) == FALSE)
+              {}
+              else
+              {
+                while($row = mysqli_fetch_row($result))
+                {
+                  echo "<tr>";
+                  echo "<td>";
+                  // họ
+                  echo "$row[0]";
+                  echo "</td>";
+                  echo "<td>";
+                  // tên
+                  echo "$row[1]";
+                  echo "</td>";
+                  echo "<td>";
+                  // giới tính
+                  echo "$row[2]";
+                  echo "</td>";
+                  echo "<td>";
+                  // ngày sinh
+                  echo "$row[3]";
+                  echo "</td>";
+                  echo "<td>";
+                  // email
+                  echo "$row[4]";
+                  echo "</td>";
+                  echo "<td>";
+                  // tài khoản
+                  echo "$row[5]";
+                  echo "</td>";
+                  echo "<td>";
+                  // nhóm
+                  echo "$row[6]";
+                  echo "</td>";
+                  echo "</tr>";
+                } 
+              }
+              mysqli_free_result($result);
+              mysqli_close($conn);
+            ?>
+          </tbody>
+        </table>
+      </div>
+      <!-- /.box-body -->
+    </div>
+    <!-- /.box -->
+  </div>
+  <!-- /.col -->
 </div>
