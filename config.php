8b8ace6677ff7998da04d9008a2dde843b2d4796
nguyenquocdat <boelovenhan@gmail.com>
2017-11-24 09:17:45 -0500

    thay doi config.php de su dung voi webservice

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <meta charset="utf-8">
 <?php
 	session_start();
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASS', '');
-	define('DB_NAME', 'group');
-	// kết nối
-	$conn = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME)or die("can't connect database".DB_HOST);
-	mysqli_set_charset($conn,'utf8');
+	define('URL_LOGIN','http://localhost/groups/webservice/?tag=login');
+	define('URL_THEMNHOM','http://localhost/groups/webservice/?tag=themnhom');
+	define('URL_DANGKYTAIKHOAN','http://localhost/groups/webservice/?tag=dangkytaikhoan');
+	define('URL_THEMNGUOIDUNG','http://localhost/groups/webservice/?tag=themnguoidung');
+	define('URL_DANHSACHTHANHVIEN', 'http://localhost/groups/webservice/?tag=danhsachthanhvien');
+	define('URL_DANHSACHNHOM', 'http://localhost/groups/webservice/?tag=danhsachnhom');
+	define('URL_THONGTINCANHAN', 'http://localhost/groups/webservice/?tag=thongtincanhan');
+	define('URL_DANHSACHCUNGNHOM', 'http://localhost/groups/webservice/?tag=danhsachcungnhom');
 ?>
